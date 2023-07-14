a96617a1de9b0007d183f0f5ad9a975e5a0d8d05
Alexandre Samson <alexander_samson@icloud.com>
2023-07-14 15:12:06 +0200

    Date class Product companion object fun getPredefinedProducts() zugefügt AccountManagement productsList angepasst

--- src/main/kotlin/Product.kt
+++ src/main/kotlin/Product.kt
@@ -1,5 +1,6 @@
 data class Product(
     val name: String,
+    val autor: String,
     var price: Double,
     var quantity: String,
     val category: String,
@@ -8,14 +9,14 @@ data class Product(
     companion object {
         fun getPredefinedProducts(): List<Product> {
             return listOf(
-                Product("Wenn der Wind singt. Pinball 1973", 25.0, "15", "Roman", "Popkultur in Japan"),
-                Product("Wilde Schafsjagd", 25.0, "20", "Roman", "Detektivgeschichte"),
-                Product("Hard-Boiled Wonderland und das Ende der Welt", 25.0, "17", "Roman", "Phantasy"),
-                Product("Tanz mit dem Schafsmann", 25.0, "12", "Roman", "Mystisch und Real"),
-                Product("1Q84", 25.0, "34", "Roman", "Phantasy"),
-                Product("Die Ermordung des Commendatore", 25.0, "10", "Roman", "Phantasy"),
-                Product("Sputnik Sweetheart", 25.0, "2", "Roman", "Mystisch und Real"),
-                Product("Gefährliche Geliebte / Südlich der Grenze, westlich der Sonne", 25.0, "4", "Roman", "Liebe und Leidenschaft")
+                Product("Wenn der Wind singt. Pinball 1973", "Haruki Murakami", 25.0, "15", "Roman", "Popkultur in Japan"),
+                Product("Wilde Schafsjagd", "Haruki Murakami", 25.0, "20", "Roman", "Detektivgeschichte"),
+                Product("Hard-Boiled Wonderland und das Ende der Welt", "Haruki Murakami", 25.0, "17", "Roman", "Phantasy"),
+                Product("Tanz mit dem Schafsmann", "Haruki Murakami", 25.0, "12", "Roman", "Mystisch und Real"),
+                Product("1Q84", "Haruki Murakami", 49.99, "34", "Roman", "Phantasy"),
+                Product("Die Ermordung des Commendatore", "Haruki Murakami", 49.99, "10", "Roman", "Phantasy"),
+                Product("Sputnik Sweetheart", "Haruki Murakami", 22.0, "2", "Roman", "Mystisch und Real"),
+                Product("Gefährliche Geliebte / Südlich der Grenze, westlich der Sonne", "Haruki Murakami", 25.0, "4", "Roman", "Liebe und Leidenschaft")
             )
         }
     }
