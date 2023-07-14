a7ab690312fc1c9309a24da8d8d3afb052caeac0
Alexandre Samson <alexander_samson@icloud.com>
2023-07-14 14:32:24 +0200

    Date class Product companion object fun getPredefinedProducts() zugefügt AccountManagement productsList angepasst

--- src/main/kotlin/Product.kt
+++ src/main/kotlin/Product.kt
@@ -8,8 +8,14 @@ data class Product(
     companion object {
         fun getPredefinedProducts(): List<Product> {
             return listOf(
-                Product("Pr1", 10.0, "10", "Kateg1", "UnterKateg1")
-                //weitere Produkte
+                Product("Wenn der Wind singt. Pinball 1973", 25.0, "15", "Kateg1", "UnterKateg1"),
+                Product("Wilde Schafsjagd", 25.0, "20", "Kateg1", "UnterKateg1"),
+                Product("Hard-Boiled Wonderland und das Ende der Welt", 25.0, "17", "Kateg1", "UnterKateg1"),
+                Product("Tanz mit dem Schafsmann", 25.0, "12", "Kateg1", "UnterKateg1"),
+                Product("1Q84", 25.0, "34", "Kateg1", "UnterKateg1"),
+                Product("Die Ermordung des Commendatore", 25.0, "10", "Kateg1", "UnterKateg1"),
+                Product("Sputnik Sweetheart", 25.0, "2", "Kateg1", "UnterKateg1"),
+                Product("Gefährliche Geliebte / Südlich der Grenze, westlich der Sonne", 25.0, "4", "Kateg1", "UnterKateg1")
             )
         }
     }
